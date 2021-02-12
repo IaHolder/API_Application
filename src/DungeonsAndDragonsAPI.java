e7e341fbebed216d3f1df1d238b600bc33fda9a9
Ian Holder <iaholder@uncg.edu>
2021-02-12 18:18:15 -0500

    finished prototype that needs to be cleaned up and style guide, changing to a open api

---
+++ src/DungeonsAndDragonsAPI.java
@@ -0,0 +1,43 @@
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.*;
+
+public class DungeonsAndDragonsAPI {
+
+    public static void getInformation(){
+        String baseURL = "https://www.dnd5eapi.co/api/";
+        String searchCatagory = "spells";
+        String searchName = "acid-arrow";
+        String urlString = baseURL + searchCatagory + "/" + searchName + "/";
+        URL url;
+        try{
+            url = new URL(urlString);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+            int status = con.getResponseCode();
+            if (status != 200) {
+                System.out.printf("Error: Could not complete search: " + status);
+            } else {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuffer content = new StringBuffer();
+                while ((inputLine = in.readLine()) != null) {
+                    content.append(inputLine);
+                }
+                in.close();
+                con.disconnect();
+                System.out.println("Output: " + content.toString());
+                JSONObject obj = new JSONObject(content.toString());
+                String description = obj.getString("name");
+                System.out.println(description);
+            }
+        }catch (Exception ex) {
+            System.out.println("Error: " + ex);
+            return;
+        }
+    }
+}
--- src/MathAPI.java
+++
@@ -1,6 +0,0 @@
-import org.json.JSONObject;
-
-
-public class MathAPI {
-
-}
